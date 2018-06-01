3d01363567622470433e0455beedc55e3d193f89
Loc Nguyen <lochnguyen@gmail.com>
2018-05-31 17:03:27 -0700

    Fix module resolution for test and development time not using module-alias in those cases

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import 'module-alias/register';
+if (process.env.NODE_ENV !== 'development') {
+    require('module-alias/register'); // tslint:disable-line
+}
+
 import { square } from '~/modules/square';
 
 console.log(`4^2 = ${square(4)}`);
