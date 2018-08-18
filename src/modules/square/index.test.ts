022b5fba03677b176a3911f894327ae31f4d132a
Loc Nguyen <lochnguyen@gmail.com>
2018-08-18 08:41:53 -0500

    Closes #32 by switching this project to use Jest instead of Mocha/Chai/Sinon

--- src/modules/square/index.test.ts
+++ src/modules/square/index.test.ts
@@ -1,9 +1,8 @@
 import { square } from '~/modules/square';
-import { expect } from 'chai';
 
 describe('A squaring function', () => {
     it('should square the argument and return the result', () => {
         const result = square(8);
-        expect(result).to.be.equal(64);
+        expect(result).toBe(64);
     });
 });
